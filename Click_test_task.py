a568cf7fab8b24657939d04b3070286c7c5e1d38
renat-ibragimov-it <renatibragimov26@gmail.com>
2022-07-08 12:07:55 +0300

    Final update of click test task

--- Click_test_task.py
+++ Click_test_task.py
@@ -1,41 +1,13 @@
-# Во вложении файл csv с данными про аэропорты мира, написать программу которая
-# сможет вернуть данные по таким параметрам:
-#
-# --iata_code - код аэропорта, должно вернуть 1 запись по коду
-# аэропорта(всю строку) или вернуть ошибку AirportNotFoundError
-# --country - страна, должно вернуть все записи по аэропортам или
-# CountryNotFoundError
-# --name - значение имени аэропорта, допустимо вхождение строки хотябы
-# минимально, т.е. liman должен вернуть строки с такими названиями:
-# Ilimanaq Heliport
-# Sidi Slimane Airport
-# Kilimanjaro International Airport
-# West Kilimanjaro Airport
-# Limanske Airfield
-# Liman Airfield
-# ...
-# или AirportNotFoundError
-#
-# Только один параметр обязателен, если выбрано несколько - вернуть
-# ошибку:
-# MultipleOptionsError, если ни одного - NoOptionsFoundError
-# ** доп. ошибки принимают два аргумента, текст ошибки и входные данные,
-#
-# пример:
-#
-# AirportNotFoundError: ('Airport not found', 'OESD')
-# CountryNotFoundError: ('Country not found', 'UGUGU')
-# IATA код может быть только 3х буквенным в верхнем регистре, сделать валидацию
-# на него или вернуть IATACodeError
-import argparse
 import csv
 import re
+
 import click
 
 
 class NoOptionsFoundError(Exception):
     """This error will be raised in case no argument received during
        program call"""
+
     def __init__(self):
         self.txt = "One argument required."
 
@@ -46,6 +18,7 @@ class NoOptionsFoundError(Exception):
 class MultipleOptionsError(Exception):
     """This error will be raised in case more than one argument received during
            program call"""
+
     def __init__(self, num):
         self.txt = f'{num} arguments entered. Only one argument required.'
 
@@ -56,6 +29,7 @@ class MultipleOptionsError(Exception):
 class IATACodeError(Exception):
     """This error will be raised in case of incorrect IATA code format will be
         entered during program call"""
+
     def __init__(self, iata_code):
         self.txt = f'"{iata_code}" is incorrect format. Should be three ' \
                    f'capital letters.'
@@ -67,6 +41,7 @@ class IATACodeError(Exception):
 class AirportNotFoundError(Exception):
     """This error will be raised in case airport name or IATA Code will not be
         found in the CSV file"""
+
     def __init__(self, parameter):
         self.txt = f'"{parameter}", Airport not found'
 
@@ -77,6 +52,7 @@ class AirportNotFoundError(Exception):
 class CountryNotFoundError(Exception):
     """This error will be raised in case country name will not be found in
         the CSV file"""
+
     def __init__(self, country):
         self.txt = f'"{country}", Country not found'
 
@@ -90,6 +66,7 @@ class CountryNotFoundError(Exception):
 @click.option('-n', '--name', help="Airport name to search")
 def main(iata_code, country, name):
     """This is the main function, the call of which will run the program"""
+
     def check_args_quantity():
         """This function will check quantity of entered arguments. According to
          the conditions of the task, there is only ony argument required.
@@ -120,6 +97,7 @@ def main(iata_code, country, name):
 class AirportSearch:
     """Class for searching of required airports in CSV file. Depending on the
         received argument, the required function will be called."""
+
     def __init__(self, iata_code, country, name):
         self.iata_code = iata_code
         self.country = country
@@ -154,9 +132,9 @@ class AirportSearch:
         raise AirportNotFoundError(self.iata_code)
 
     def find_country(self):
-        """This function will only be called if an Country argument is
+        """This function will only be called if a Country argument is
         received. It will return all rows from CSV file containing requested
-        country. CountryNotFoundError will be raised in case contry will
+        country. CountryNotFoundError will be raised in case country will
         not be found in the CSV file"""
         airports_in_country = []
         for row in self.extract_info():
@@ -185,4 +163,3 @@ class AirportSearch:
 
 if __name__ == '__main__':
     main()
-
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Argparse_-_Click
-A test task aimed at understanding the basic principles of working with the Argparse &amp; Click
+A test task aimed at understanding the basic principles of working with the 
+Argparse and Click.
+
+# Details
+The purpose of this task is to extract rows matching the conditions given in
+arguments from a CSV file. CSV file containing the list of airports around the
+world. There are three possible arguments: IATA code, Country name and Airport
+name. 
+
+IATA code should contain only three capital letters in format "AAA","BBB", etc. 
+
+Country name should contain two capital letters (international format) 
+in format "UA", "UK", "US", etc. Program will find and return all matches. 
+
+Airport name should contain name (or part of the name) of airport. Program will 
+find and return all matches. For example if Airport name argument will be 
+"liman", program will return "Ilimanaq Heliport", "Sidi Slimane Airport", 
+"Kilimanjaro International Airport", etc.
+
+We should use only one of the three available arguments. To create 
+arguments I used Argparse and Click (in the separate modules).
+
+Also, custom errors were created to show IATACodeError if incorrect IATA code 
+format will be entered, MultipleOptionsError if more than one argument 
+received, etc.
+
